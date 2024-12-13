dff3d6647fec8e3d5cb2a31f02aeda0bfbff500a
NafiulHasan41 <nhashik40@gmail.com>
2024-12-13 09:17:45 +0600

    Updated video link

--- src/controllers/tutorialController.ts
+++ src/controllers/tutorialController.ts
@@ -3,14 +3,14 @@ import { Request, Response } from "express";
 
 export const getTutorialVideos = async (req: Request, res: Response) => {
   const tutorialVideos = [
-    { title: "Japanese Greetings", url: "https://www.youtube.com/embed/XYZ123" },
-    { title: "Learn Hiragana in 30 minutes", url: "https://www.youtube.com/embed/ABC456" },
-    { title: "Basic Japanese Phrases", url: "https://www.youtube.com/embed/DEF789" },
-    { title: "Japanese Greetings", url: "https://www.youtube.com/embed/XYZ123" },
-    { title: "Learn Hiragana in 30 minutes", url: "https://www.youtube.com/embed/ABC456" },
-    { title: "Basic Japanese Phrases", url: "https://www.youtube.com/embed/DEF789" },
-    { title: "Master Katakana", url: "https://www.youtube.com/embed/GHI012" },
-    { title: "Common Japanese Expressions", url: "https://www.youtube.com/embed/JKL345" },
+    { title: "N5 Vocabulary Lesson-1", url: "https://www.youtube.com/embed/BgzBHTgp4K0?si=mGxF8iX6yPL9BcMF" },
+    { title: "Learn Hiragana in 30 minutes", url: "https://www.youtube.com/embed/rGJEV1eS2mg?si=-a4uEk1wbTC5ZVnt" },
+    { title: "Basic Japanese Phrases", url: "https://www.youtube.com/embed/vuly0ZEo6wo?si=Ow5f5-MWG13WB12E" },
+    { title: "Japanese Greetings", url: "https://www.youtube.com/embed/81c5U9WKET0?si=YhLG9FBZcuvYD1lC" },
+    { title: "Learn Hiragana in 30 minutes", url: "https://www.youtube.com/embed/DCxPOpV8Yaw?si=WwGI5Z-iIvyMz_De" },
+    { title: "Basic Japanese Phrases", url: "https://www.youtube.com/embed/M5AqMcOyWdg?si=x-AJmYWAHnUUZjHR" },
+    { title: "Master Katakana", url: "https://www.youtube.com/embed/pi40ibPeD64?si=tq_gTxiBS0J6XX6Y" },
+    { title: "Common Japanese Expressions", url: "https://www.youtube.com/embed/ljrnyDhVsgs?si=7wDmYTR-CEohNadx" },
   ];
   res.status(200).json(tutorialVideos);
 };
